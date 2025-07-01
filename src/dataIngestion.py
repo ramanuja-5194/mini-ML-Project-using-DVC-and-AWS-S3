6ae23b09834288c69bf586d6a1a137c998720326
L A V Ramanuja <ramanujalakkimsetti@gmail.com>
2025-07-01 12:37:45 +0530

    params configured for the pipeline

--- src/dataIngestion.py
+++ src/dataIngestion.py
@@ -33,7 +33,7 @@ def load_params(params_path: str) -> dict:
     try:
         with open(params_path, 'r') as file:
             params = yaml.safe_load(file)
-        logger.debug('Parameters retrieved from %s', params_path)
+        logger.debug('Parameters retrieved from %s', params_path) # this function just retrives parameters from yaml file.
         return params
     except FileNotFoundError:
         logger.error('File not found: %s', params_path)
@@ -86,9 +86,9 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        # params = load_params(params_path='params.yaml')
-        # test_size = params['data_ingestion']['test_size']
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['dataIngestion']['test_size'] # taking parameters values similar to dictionary.
+        #test_size = 0.2
         data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
